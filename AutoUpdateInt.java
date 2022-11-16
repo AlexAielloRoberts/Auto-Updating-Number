57737e58e66439a23ce55f2c7fa644d4e76330a8
Alex Roberts <118322842+AlexAielloRoberts@users.noreply.github.com>
2022-11-15 21:48:37 -0500

    Initial commit

---
+++ AutoUpdateInt.java
@@ -0,0 +1,17 @@
+public class AutoUpdateInt
+{
+  private int value;
+
+  public AutoUpdateInt()
+  {
+    value = 0;
+  }
+
+  public AutoUpdateInt(int initialValue)
+  {
+    value = 0;
+  }
+
+
+
+}
